c5bccedc1a48a524a42249c20ec55fb26f8a3b32
Bruno Michel <brunomichel00@gmail.com>
2018-07-09 18:24:09 -0300

    Initial commit

---
+++ app/src/main/java/com/brunopera/animations/MainActivity.java
@@ -0,0 +1,32 @@
+package com.brunopera.animations;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public void fade(View view){
+
+        ImageView bart = (ImageView) findViewById(R.id.imgBart);
+        ImageView homer = (ImageView) findViewById(R.id.imgHomer);
+
+
+
+        bart.animate().alpha(0f).setDuration(2000);
+
+        homer.animate().alpha(1f).setDuration(2000);
+
+    }
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
